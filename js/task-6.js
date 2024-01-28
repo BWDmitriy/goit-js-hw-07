b61f10db531539566966d47503bf35f730410e39
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-01-28 19:55:40 +0200

    Task 6 completed

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const boxes = document.querySelector('#boxes');
+const controls = document.querySelector('#controls');
+const numberInput = document.querySelector('input#input-value');
+const createBtn = document.querySelector('#create-button');
+const destroyBtn = document.querySelector('#destroy-button');
+
+let boxAmount = 0;
+numberInput.addEventListener('input', e => {
+  boxAmount = e.target.value;
+});
+
+createBtn.addEventListener('click', createBoxes);
+function createBoxes() {
+  if (boxAmount > 0 && boxAmount <= 100) {
+    for (let i = 0; i < boxAmount; i++) {
+      const newBox = document.createElement('div');
+      newBox.style.backgroundColor = getRandomHexColor();
+      newBox.style.width = 30 + 10 * boxes.children.length + 'px';
+      newBox.style.height = 30 + 10 * boxes.children.length + 'px';
+      newBox.className = 'box';
+      boxes.append(newBox);
+    }
+  }
+}
+destroyBtn.addEventListener('click', destroyBoxes);
+function destroyBoxes() {
+  const boxesList = document.querySelectorAll('.box');
+  for (const box of boxesList) {
+    box.remove();
+  }
+}
