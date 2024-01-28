7c7383fcbb09a2e68e89119d89b09fc8fbe4d168
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-01-28 17:51:10 +0200

    Task 5 completed

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,13 @@
+const colorButton = document.querySelector('button.change-color');
+const spanColor = document.querySelector('span.color');
+
+const changeColor = () => {
+  let randomizedColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomizedColor;
+  spanColor.textContent = randomizedColor;
+};
+colorButton.addEventListener('click', changeColor);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
