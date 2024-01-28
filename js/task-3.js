bafad7a34056840c5aebe6abcbcea21d98a195e2
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-01-28 17:39:10 +0200

    Task 3, 4 completed

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,6 @@
-addEventListener('change', event => {});
+const inputField = document.querySelector('input#name-input');
+const outputField = document.querySelector('span#name-output');
+
+inputField.addEventListener('input', e => {
+  outputField.textContent = e.target.value;
+});
