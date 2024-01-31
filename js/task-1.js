2a164ce6b18a4e24955134d1874208a260975e94
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-01-31 18:24:02 +0200

    Task 1 QoL

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,10 @@
 const categoryList = document.querySelector('ul#categories');
-console.log(categoryList);
-const categoryNumber = categoryList.children.length;
-// const categoryNumber = document.querySelectorAll('li.item').length;
+const categoryNumber = document.querySelectorAll('li.item').length;
 console.log('Number of categories: ' + categoryNumber);
 const categories = document.querySelectorAll('li.item');
 categories.forEach(item => {
   let categoryName = item.querySelector('h2');
-  let categoryElements = item.querySelectorAll('li').length;
+  let categoryElementsCount = item.querySelectorAll('li').length;
   console.log('Category: ' + categoryName.textContent);
-  console.log('Elements: ' + categoryElements);
+  console.log('Elements: ' + categoryElementsCount);
 });
