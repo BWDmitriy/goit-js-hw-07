c2bb67c1006313d81203cebebdc4e0ed0caee2c9
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-02-01 18:01:09 +0200

    Task 5 QoL

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,9 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
 const colorButton = document.querySelector('button.change-color');
 const spanColor = document.querySelector('span.color');
 
@@ -7,9 +13,3 @@ const changeColor = () => {
   spanColor.textContent = randomizedColor;
 };
 colorButton.addEventListener('click', changeColor);
-
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
