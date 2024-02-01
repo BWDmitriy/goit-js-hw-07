73e30630ba8c672b7267c5e8c669742bbf3a8a4d
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-02-01 16:58:23 +0200

    task 3 fixed

--- js/task-3.js
+++ js/task-3.js
@@ -3,4 +3,7 @@ const outputField = document.querySelector('span#name-output');
 
 inputField.addEventListener('input', e => {
   outputField.textContent = e.target.value;
+  if (!e.target.value) {
+    outputField.textContent = 'Anonymous';
+  }
 });
